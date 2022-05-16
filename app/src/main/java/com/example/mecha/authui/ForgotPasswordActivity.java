1c8730672cb86ef8f4813591ae1b4ca35cb386bb
FahmiFauzi26 <fahmifauziabdullah26@gmail.com>
2022-05-16 21:42:22 +0700

    logic resetP

--- app/src/main/java/com/example/mecha/authui/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/mecha/authui/ForgotPasswordActivity.java
@@ -1,17 +1,28 @@
 package com.example.mecha.authui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.mecha.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    EditText et_email;
     Button resetPasswordButton;
+    ImageButton btn_back;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +30,46 @@ public class ForgotPasswordActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forgot_password);
 
         resetPasswordButton = findViewById(R.id.resetPasswordButton);
+        btn_back = findViewById(R.id.btn_back);
+        et_email = findViewById(R.id.et_email);
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ForgotPasswordActivity.this, LoginActivity.class));
+            }
+        });
 
         resetPasswordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+                resetPassword();
+            }
+        });
+    }
+    private void resetPassword(){
+        String email = et_email.getText().toString().trim();
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            et_email.setError("Email tidak valid !");
+            et_email.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            et_email.setError("Tidak boleh kosong !");
+            et_email.requestFocus();
+            return;
+        }
+//        progressBar.setVisibility(View.VISIBLE);
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ForgotPasswordActivity.this, "Silahkan cek email anda", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(ForgotPasswordActivity.this,"Gagal mengirim email",Toast.LENGTH_LONG).show();
+                }
             }
         });
     }
