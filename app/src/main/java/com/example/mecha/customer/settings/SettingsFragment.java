4e2c1cbdf5da42af0dfbf9e383c8f1bfe64f4fda
FahmiFauzi26 <fahmifauziabdullah26@gmail.com>
2022-06-15 06:12:44 +0700

    upload profilePicture

--- app/src/main/java/com/example/mecha/customer/settings/SettingsFragment.java
+++ app/src/main/java/com/example/mecha/customer/settings/SettingsFragment.java
@@ -1,27 +1,50 @@
 package com.example.mecha.customer.settings;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.mecha.R;
+import com.example.mecha.authui.SignUpCustomerActivity;
+import com.example.mecha.customer.CustomerMenuActivity;
 import com.example.mecha.customer.home.EmergencyFixActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 public class SettingsFragment extends Fragment {
 
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore mstore;
+    private FirebaseUser user;
+    private StorageReference storageReference;
+
+    ImageView profilePicture;
     ImageButton btnEdit;
     RelativeLayout btn_changePassword;
     RelativeLayout btn_helpCenter;
@@ -31,6 +54,7 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -38,6 +62,22 @@ public class SettingsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+        mAuth = FirebaseAuth.getInstance();
+        mstore = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        StorageReference profileRef = storageReference.child("Users/"+mAuth.getCurrentUser().getUid()+"/profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profilePicture);
+            }
+        });
+
+
+
+
 
         btnEdit = view.findViewById(R.id.btn_edit);
 
@@ -66,7 +106,52 @@ public class SettingsFragment extends Fragment {
             }
         });
 
+        profilePicture = view.findViewById(R.id.profilePicture);
+        profilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent, 1000);
+            }
+        });
+
+
         return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+//                profilePicture.setImageURI(imageUri);
+
+                uploadImageToFirebase(imageUri);
+
+            }
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        StorageReference fileRef = storageReference.child("Users/"+mAuth.getCurrentUser().getUid()+"/profile.jpg");
+
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profilePicture);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(), "Gagal !", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void showLogOutAlertDialog() {
