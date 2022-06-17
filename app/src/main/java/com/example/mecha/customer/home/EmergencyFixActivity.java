9b6910d3cb9cb5716bc915f4f1489d20fa415b71
Kevin <kevinraihanr45@gmail.com>
2022-06-17 21:11:23 +0700

    added overlay

--- app/src/main/java/com/example/mecha/customer/home/EmergencyFixActivity.java
+++ app/src/main/java/com/example/mecha/customer/home/EmergencyFixActivity.java
@@ -1,8 +1,16 @@
 package com.example.mecha.customer.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.ImageButton;
 
 import com.example.mecha.R;
@@ -10,6 +18,7 @@ import com.example.mecha.R;
 public class EmergencyFixActivity extends AppCompatActivity {
 
     ImageButton btnback;
+    CardView mogokBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +27,27 @@ public class EmergencyFixActivity extends AppCompatActivity {
 
         btnback = findViewById(R.id.btnBack);
         btnback.setOnClickListener(view -> finish());
+
+        mogokBtn = findViewById(R.id.mogokBtn);
+        mogokBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+
+            private void showDialog() {
+
+                final Dialog dialog = new Dialog(EmergencyFixActivity.this);
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.bottomsheetlayout);
+
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
+
+            }
+        });
     }
 }
