8101cc10d17b9158e865ef76228fa9b4b5ddf1bd
Kevin <kevinraihanr45@gmail.com>
2022-07-21 09:54:58 +0800

    added false image

--- app/src/main/java/com/example/mecha/authui/testaja.java
+++ app/src/main/java/com/example/mecha/authui/testaja.java
@@ -1,9 +1,11 @@
 package com.example.mecha.authui;
 
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class testaja {
     Button bu;
     Button scsss;
     Button test;
+    ImageView pantek;
 }
